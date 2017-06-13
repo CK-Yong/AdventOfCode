b55893c4ca43f3591d5b2b97ef1a69549c31a559
CKYong <ckyong@sogyo.nl>
2017-06-13 21:38:45 +0200

    Set up files for day4 programming.

---
+++ src/main/kotlin/AdventOfCodeDay4/Day4.kt
@@ -0,0 +1,8 @@
+package AdventOfCodeDay4
+
+/**
+ * Created by CKTox on 13-Jun-17.
+ */
+fun main(args: Array<String>) {
+
+}
---
+++ src/test/kotlin/AdventOfCodeDay4/Day4Test.kt
@@ -0,0 +1,6 @@
+package AdventOfCodeDay4
+
+/**
+ * Created by CKTox on 13-Jun-17.
+ */
+class Day4Test
